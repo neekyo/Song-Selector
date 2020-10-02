ee7aae0654d7a36dc8e7295ed53cb5c170dfb1ef
Nick Sherman <ogleffeckt2@yahoo.com>
2020-10-02 08:49:00 -0400

    fixing broken git folder

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
+
+const App = () => {
+	return (
+		<div className="ui container grid">
+			<div className="ui row">
+				<div className="column eight wide list">
+					<SongList />
+				</div>
+				<div className="column eight wide list">
+					<SongDetail />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+	return [
+		{ title: 'No Scrubs', duration: '4:09' },
+		{ title: 'Macarena', duration: '3:50' },
+		{ title: 'All Star', duration: '3:57' },
+		{ title: 'I Want it That Way', duration: '3:40' }
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
+
+export default combineReducers({
+	songs: songsReducer,
+	selectedSong: selectedSongReducer
+});
